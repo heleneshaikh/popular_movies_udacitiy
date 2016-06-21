1dfe1aca1ee8ec24425aceeee124319a8ccc2d0c
heleneshaikh <helene.shaikh@gmail.com>
2016-06-21 11:16:07 +0200

    add touch event imageView

--- app/src/main/java/com/hfad/popularmovies/PosterAdapter.java
+++ app/src/main/java/com/hfad/popularmovies/PosterAdapter.java
@@ -1,9 +1,11 @@
 package com.hfad.popularmovies;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -36,6 +38,30 @@ public class PosterAdapter extends RecyclerView.Adapter<PosterAdapter.ViewHolder
         public ViewHolder(View view) {
             super(view);
             imageView = (ImageView) view.findViewById(R.id.iv_cardview);
+            imageView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN: {
+                            ImageView view = (ImageView) v;
+                            //overlay is black with transparency of 0x77 (119)
+                            view.getDrawable().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+                            view.invalidate();
+                            break;
+                        }
+                        case MotionEvent.ACTION_UP:
+                        case MotionEvent.ACTION_CANCEL: {
+                            ImageView view = (ImageView) v;
+                            //clear the overlay
+                            view.getDrawable().clearColorFilter();
+                            view.invalidate();
+                            break;
+                        }
+                    }
+
+                    return true;
+                }
+            });
         }
     }
 
